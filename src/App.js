a16b7006a88210ecb5d928f7657c0e2eb6ccf12a
joe <joe.ulica@gmail.com>
2022-12-01 15:42:00 -0500

    finished product - forgot to use git

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from "./Components/Footer";
+import Main from "./Components/Main";
+import Info from "./Components/Info"
+
+function App() {
+  return (
+    <div className="App">
+      <Info />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
